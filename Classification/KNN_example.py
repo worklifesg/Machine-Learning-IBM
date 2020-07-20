f21ad40902610236c78c0278d85bd311ed5585b7
Shraman Gupta <68037605+worklifesg@users.noreply.github.com>
2020-07-20 13:58:16 -0400

    Update KNN_example.py

--- Classification/KNN_example.py
+++ Classification/KNN_example.py
@@ -26,7 +26,6 @@ with open('KNN.txt','a') as f:
 
 
 df.hist(column='income',bins=50)
-plt.show()
 
 ##Feature Set X,Y##
 
@@ -45,4 +44,23 @@ X[0:5]
 # Train Test Split#
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=4)
-print('Train set: ', X_train.)
+with open('KNN.txt','a') as f:
+    print('Train set: ', X_train.shape,y_train.shape,file=f)
+    print('Test set: ', X_test.shape,y_test.shape,file=f)
+
+# Classification - K nearest neighbour#
+
+from sklearn.neighbors import KNeighborsClassifier
+
+k=9 # Train model and predict with k=9 (best value for high accuracy, check KNN_k_values.py program)
+neigh=KNeighborsClassifier(n_neighbors=k).fit(X_train,y_train)
+yhat=neigh.predict(X_test)
+with open('KNN.txt','a') as f:
+    print(yhat[0:5],file=f)
+
+#Predict using accuracy_score which is similar to jaccard_similarity_score function#
+
+from sklearn import metrics
+with open('KNN.txt','a') as f:
+    print('Train set accuracy: ', metrics.accuracy_score(y_train,neigh.predict(X_train)),file=f)
+    print('Test set accuracy: ',metrics.accuracy_score(y_test,yhat),file=f)
