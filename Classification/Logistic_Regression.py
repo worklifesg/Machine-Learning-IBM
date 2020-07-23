f835878e8c1a8b72440330a0da31b8e18b1fcd68
Shraman Gupta <68037605+worklifesg@users.noreply.github.com>
2020-07-23 11:43:03 -0400

    Update Logistic_Regression.py

--- Classification/Logistic_Regression.py
+++ Classification/Logistic_Regression.py
@@ -1,5 +1,3 @@
-# In progress. Not completed yet.
-
 import pandas as pd 
 import numpy as np
 import scipy.optimize as opt
@@ -55,6 +53,61 @@ yhat_prob=LogReg.predict_proba(X_test) # predict_proba returns estimates for all
 
 ### Evaluation ##
 
-from sklearn.metrics import jaccard_similarity_score
+from sklearn.metrics import jaccard_score
+from sklearn.metrics import accuracy_score
 with open('Log_Reg.txt','a') as f:
-    print('J Score: ', jaccard_similarity_score(y_test,yhat),file=f)
+    print('J Score: ', jaccard_score(y_test,yhat,labels=None, average='binary', sample_weight=None),file=f)
+    print('Accuracy Score: ', accuracy_score(y_test,yhat),file=f)
+
+#Note: In current version of scikit-learn 0.23.1 jaccard_similarity_score is replaced by jaccard_score which
+#differs by definition as jaccard_similarity_score is just same as accuracy_score but by definition of 
+#jaccard index accuracy score and jaccard score are different. SO, if using scikit-learn > 0.23.1, jaccard_similarity_score not available
+
+#Using Confusion matrix #
+
+from sklearn.metrics import classification_report,confusion_matrix
+import itertools
+
+def plot_cmat(cm,classes,normalize=False,
+                title='Confusion Matrix',cmap=plt.cm.Blues):
+                if normalize:
+                    cm=cm.astype('float')/cm.sum(axis=1)[:,np.newaxis]
+                    print('Normalized Confusion Matrix')
+                else:
+                    print('COnfusion matrix without Normalization')
+                
+                with open('Log_Reg.txt','a') as f:
+                    print(cm,file=f)
+
+                plt.imshow(cm,interpolation='nearest',cmap=cmap)
+                plt.title(title)
+                plt.colorbar()
+                tick_marks=np.arange(len(classes))
+                plt.xticks(tick_marks,classes,rotation=45)
+                plt.yticks(tick_marks,classes)
+
+                #For Labeling inside boxes #
+                fmt='.2f' if normalize else 'd'
+                threshold=cm.max()/2
+                for i,j in itertools.product(range(cm.shape[0]),range(cm.shape[1])):
+                    plt.text(j,i,format(cm[i,j],fmt),
+                            horizontalalignment='center',
+                            color='white' if cm[i,j] > threshold else 'black')
+                plt.tight_layout
+                plt.ylabel('True Label')
+                plt.xlabel('Predicted Label')
+
+#Confusion matrix #
+c_mat=confusion_matrix(y_test,yhat,labels=[1,0])
+with open('Log_Reg.txt','a') as f:
+    print('Confusion matrix: \n ',c_mat,file=f)
+
+#Confusion matrix plot#
+plt.figure()
+plot_cmat(c_mat,classes=['churn=1','churn=0'],normalize=False,title='Confusion Matrix')
+plt.show()
+
+# Compute classification report - Precision, Recall, F1Score and Support
+
+with open('Log_Reg.txt','a') as f:
+    print('Classification Report: \n ',classification_report(y_test,yhat),file=f)
